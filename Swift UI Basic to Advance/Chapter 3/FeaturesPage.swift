656f980e1fe97e41122bf59525e5e01fb7d2d2b6
Jeet Panchal <jeetbpanchal@gmail.com>
2025-01-15 15:46:41 -0500

    Chapter 3 Cont: Create the features page Build a second page to display a list of cards describing the app’s features.
    To keep your views organized on the features page, you’ll create two views, one for the page itself and another for the cards on the page.

---
+++ Swift UI Basic to Advance/Chapter 3/FeaturesPage.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturesPage.swift
+//  Swift UI Basic to Advance
+//
+//  Created by Jeet Panchal on 2025-01-15.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+            
+            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+}
