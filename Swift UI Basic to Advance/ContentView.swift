502e7fdb3ec0ad5bc5ef42d4e4bd9a1b60c4eaed
Jeet Panchal <jeetbpanchal@gmail.com>
2025-01-15 15:08:57 -0500

    Initial Commit

---
+++ Swift UI Basic to Advance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift UI Basic to Advance
+//
+//  Created by Jeet Panchal on 2025-01-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift UI Basic to Advance/Swift_UI_Basic_to_AdvanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_Basic_to_AdvanceApp.swift
+//  Swift UI Basic to Advance
+//
+//  Created by Jeet Panchal on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_Basic_to_AdvanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
