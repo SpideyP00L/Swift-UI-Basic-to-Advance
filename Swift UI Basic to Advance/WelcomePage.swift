9bdb42dbb5d12bad6d1c10f657dd0bc9cf0e9cad
Jeet Panchal <jeetbpanchal@gmail.com>
2025-01-15 15:31:50 -0500

    Chapter 3 Cont: Inspect the size of your views using borders Explore how SwiftUI lays out views, and how modifiers affect layout, using .border. A border outlines the area a view occupies.

--- Swift UI Basic to Advance/WelcomePage.swift
+++ Swift UI Basic to Advance/WelcomePage.swift
@@ -11,11 +11,26 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text("Welcome to MyApp")
-            .font(.title)
-            .fontWeight(.semibold)
+        VStack {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.tint)
+            
+            Text("Welcome to MyApp")
+                .font(.title)
+                .fontWeight(.semibold)
+                .border(.black, width: 1.5)
+            
+            Text("Description text")
+                .font(.title2)
+                .border(.black, width: 1.5)
+        }
+        .border(.orange, width: 1.5)
+        .padding()
+        .border(.purple, width: 1.5)
     }
 }
+
 
 #Preview {
     WelcomePage()
